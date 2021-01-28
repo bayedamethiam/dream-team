80f9686aff4d8716a88e76975d04a33408078e54
Aliou Abou DIALLO <aliou.dialo@groupe-esigelec.org>
2021-01-28 23:48:00 +0100

    bdt

--- app/admin/views.py
+++ app/admin/views.py
@@ -783,7 +783,7 @@ def cr_test(id):
                 pdfkit_config = pdfkit.configuration(wkhtmltopdf=os.environ.get('WKHTMLTOPDF_BINARY', 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'))
         else:
                 os.environ['PATH'] += os.pathsep + os.path.dirname(sys.executable) 
-                WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')], 
+                WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf-pack')], 
                     stdout=subprocess.PIPE).communicate()[0].strip()
                 pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
         
